189dfa6e5ab7fbc1802bbc74d6a79244b1567893
raryosu <hagi.1007.kosen@gmail.com>
2015-08-17 22:57:59 +0900

    :+1: アーカイブ機能を追加

--- app/js/app.js
+++ app/js/app.js
@@ -8,5 +8,16 @@ angular.module('TodoApp', [])
 			self.todos.push({title: self.newTodo, done: false});
 			self.newTodo = '';
 		};
+
+		// アーカイブ機能
+		self.archive = function() {
+			var currentTodo = self.todos;
+			self.todos = [];
+			angular.forEach(currentTodo, function(todo) {
+				if(!todo.done) {
+					self.todos.push(todo);
+				}
+			});
+		};
 	});
 
