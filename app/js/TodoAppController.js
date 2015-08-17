1807bd1b9e5e7a7023e7ba6f9596665ab4cedae4
raryosu <hagi.1007.kosen@gmail.com>
2015-08-17 23:26:39 +0900

    :art: ファイル構造を変更

---
+++ app/js/TodoAppController.js
@@ -0,0 +1,22 @@
+function TodoAppController() {
+	var self = this;
+
+	self.todos = [];
+
+	// todo作成
+	self.create = function() {
+		self.todos.push({title: self.newTodo, done: false});
+		self.newTodo = '';
+	};
+
+	// アーカイブ機能
+	self.archive = function() {
+		var currentTodo = self.todos;
+		self.todos = [];
+		angular.forEach(currentTodo, function(todo) {
+			if(!todo.done) {
+				self.todos.push(todo);
+			}
+		});
+	};
+}
--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,3 @@
 angular.module('TodoApp', [])
-	.controller('TodoAppController', function() {
-		var self = this;
-		self.todos = [];
-
-		// todo作成
-		self.create = function() {
-			self.todos.push({title: self.newTodo, done: false});
-			self.newTodo = '';
-		};
-
-		// アーカイブ機能
-		self.archive = function() {
-			var currentTodo = self.todos;
-			self.todos = [];
-			angular.forEach(currentTodo, function(todo) {
-				if(!todo.done) {
-					self.todos.push(todo);
-				}
-			});
-		};
-	});
+	.controller('TodoAppController', TodoAppController);
 
