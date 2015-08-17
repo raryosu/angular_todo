efdf3ab6c950e7c5214680381dad180cdaa8b629
raryosu <hagi.1007.kosen@gmail.com>
2015-08-17 22:23:28 +0900

    :+1: todoを追加できるようにした

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,11 @@
-angular.module('App', []);
+angular.module('TodoApp', [])
+	.controller('TodoAppController', function() {
+		var self = this;
+		self.todos = [];
+
+		// todo作成
+		self.create = function() {
+			self.todos.push(self.newTodo);
+			self.newTodo = '';
+		};
+	});
